27cbe8a6d3fce72be45d1872c6ba6be2b4978138
Nicolas Valcárcel Scerpella <nicolas.valcarcel@ampushsocial.com>
2012-10-10 13:42:28 -0500

    remove unneeded code; pep8; readability changes

--- extract_values.py
+++ extract_values.py
@@ -3,7 +3,7 @@ A Python module for extracting values out of a string using a simple pattern
 of named group of characters delimited by a pair of opening and closing
 delimiters as a simpler alternative to using regular expressions.
 
-E.g. 
+E.g.
 >>> from extract_values import extract_values
 >>> extract_values('John Doe <john@example.com> (http://example.com)', \
 '{name} <{email}> ({url})')
@@ -13,11 +13,13 @@ import re
 
 __version__ = '1.0'
 
+
 class error(Exception):
     pass
 
+
 def extract_values(
-    string, pattern, lowercase=False, whitespace=None, 
+    string, pattern, lowercase=False, whitespace=None,
     strip_values=False, delimiters=['{', '}']
 ):
     """
@@ -34,44 +36,54 @@ def extract_values(
     # Checking for lowercase convertion
     if lowercase:
         string = string.lower()
+
     # Checking for a sequence in delimiters
     try:
         delimiters = tuple(delimiters)
     except TypeError, e:
         raise error('Error with delimiters since %s' % e)
+
     # Checking for just two delimiters
     if len(delimiters) != 2:
         raise error('delimiters must be a sequence with just two characters')
+
     # Check if whitespace should be removed
-    whitespace_error_message = ''.join([
+    whitespace_error_message = (
         'whitespace must be a non-negative integer '
         'with the minimal number of continous whitespace '
         'to remove'
-    ])
+    )
+
     if whitespace is not None:
         # Make sure it's an integer
         try:
             whitespace = int(whitespace)
         except TypeError:
             raise error(whitespace_error_message)
+
         # Make sure it's a non-negative integer
         if whitespace < 0:
             raise error(whitespace_error_message)
-        if whitespace is not None:
-            # Now remove whitespace
-            if whitespace == 0:
-                # Removing all whitespace from each value
-                string = re.sub('\s',  '', string) 
-            else:
-                # Removing only extra whitespace from each value
-                string = re.sub('(\s)\s{%d,}' % (whitespace-1), '\\1', string) 
+
+        # Now remove whitespace
+        if whitespace == 0:
+            # Removing all whitespace from each value
+            string = re.sub('\s',  '', string)
+        else:
+            # Removing only extra whitespace from each value
+            string = re.sub(
+                '(\s)\s{%d,}' % (whitespace - 1), '\\1', string
+            )
+
     # Helper regular expressions
     splitter = re.compile('(%s\w+%s)' % (delimiters[0], delimiters[1]))
     extracter = re.compile('%s(\w+)%s' % (delimiters[0], delimiters[1]))
+
     # Split pattern into parts including named groups
     parts = splitter.split(pattern)
+
     # Expand group or escape non-group
-    for idx,p in enumerate(parts):
+    for idx, p in enumerate(parts):
         # Part is a named group
         if splitter.match(p):
             name = extracter.search(p).groups()[0]
@@ -80,20 +92,26 @@ def extract_values(
         else:
             # Escape values with special semantics in regular expressions
             parts[idx] = re.escape(p)
+
     # Build expanded pattern
     expanded_pattern = '^%s$' % ''.join(parts)
+
     try:
         # Attempt to extract values
         value_dict = re.match(expanded_pattern, string).groupdict()
+
         # Check if values must be stripped
         if strip_values:
             for name in value_dict:
                 value_dict[name] = value_dict[name].strip()
+
         # Finally, return values
         return value_dict
+
     except re.error, e:
         # Something went wrong, we pass along the message from re module
         raise error(e.message)
+
     except AttributeError:
         # No regexp match, just return an empty dictionary
         return {}
