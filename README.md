705121456f9abea2cae5e338c1a9bcc356810b06
Antonio Ognio <gnrfan@gnrfan.org>
2012-10-10 07:42:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Extract Values
+==============
+
+A Python module for extracting values out of a string using a simple pattern of named group of characters delimited by a pair of opening and closing delimiters as a simpler alternative to using regular expressions.
+
+Examples:
+
+    from extract_values import extract_values
+
+    >>> extract_values('/2012/08/12/test.html', '/{year}/{month}/{day}/{title}.html')
+    { 'year': '2012', 'month': '08', 'day': '12', 'title': 'test' }
+
+    >>> extract_values('John Doe <john@example.com> (http://example.com)', '{name} <{email}> ({url})')
+    {'name': 'John Doe', 'email': 'john@example.com', 'url': 'http://example.com' }
+
+    >>> extract_values('from 4th October  to 10th  October', 'from `from` to `to`', strip_values=True, whitespace=1, delimiters=['`', '`'])
+    {'from': '4th October', 'to': '10th October' }
+
+    >>> extract_values('Convert 1500 Grams to Kilograms', 'convert {quantity} {from_unit} to {to_unit}', lowercase=True)
+    {'quantity': '1500', 'from_unit': 'grams', 'to_unit': 'kilograms' }]
+
+Optional parameters:
+
+* **lowercase** - When True is passed the string is lowercased first              
+* **whitespace** - Minimal number of extra whitespace to remove                   
+* **strip_values** - When True calls strip() for each value removing whitespace   
+* **delimiters** - A sequence with only the opening and closing delimiters
+
+This little project was inspired by a similar [Node.js](http://nodejs.org/) module created by sri-lankan developer [Lakshan Perera](http://laktek.com/) available [here](https://github.com/laktek/extract-values).
+
+(c) 2012 - Antonio Ognio <antonio@ognio.com> - Distributed under the BSD license - Made in Peru.
---
+++ extract_values.py
@@ -0,0 +1,98 @@
+"""
+A Python module for extracting values out of a string using a simple pattern
+of named group of characters delimited by a pair of opening and closing
+delimiters as a simpler alternative to using regular expressions.
+
+E.g. 
+>>> from extract_values import extract_values
+>>> extract_values('John Doe <john@example.com> (http://example.com)', \
+'{name} <{email}> ({url})')
+{'url': 'http://example.com', 'name': 'John Doe', 'email': 'john@example.com'}
+"""
+import re
+
+class error(Exception):
+    pass
+
+def extract_values(
+    string, pattern, lowercase=False, whitespace=None, 
+    strip_values=False, delimiters=['{', '}']
+):
+    """
+    Extracts values from a string using a pattern made of named groups
+    of characters.
+
+    Optional parameters:
+
+    lowercase - When True is passed the string is lowercased first
+    whitespace - Minimal number of extra whitespace to remove
+    strip_values - When True calls strip() for each value removing whitespace
+    delimiters - A sequence with only the opening and closing delimiters
+    """
+    # Checking for lowercase convertion
+    if lowercase:
+        string = string.lower()
+    # Checking for a sequence in delimiters
+    try:
+        delimiters = tuple(delimiters)
+    except TypeError, e:
+        raise error('Error with delimiters since %s' % e)
+    # Checking for just two delimiters
+    if len(delimiters) != 2:
+        raise error('delimiters must be a sequence with just two characters')
+    splitter = re.compile('(%s\w+%s)' % (delimiters[0], delimiters[1]))
+    extracter = re.compile('%s(\w+)%s' % (delimiters[0], delimiters[1]))
+    # Split pattern into parts including named groups
+    parts = splitter.split(pattern)
+    # Expand group or escape non-group
+    for idx,p in enumerate(parts):
+        # Part is a named group
+        if splitter.match(p):
+            name = extracter.search(p).groups()[0]
+            parts[idx] = '(?P<%s>.+)' % name
+        # Part is something else
+        else:
+            parts[idx] = re.escape(p)
+    # Build expanded pattern
+    expanded_pattern = ''.join(parts)
+    try:
+        # Attempt to extract values
+        value_dict = re.match(expanded_pattern, string).groupdict()
+        # Check if values must be stripped
+        if strip_values:
+            for name in value_dict:
+                value_dict[name] = value_dict[name].strip()
+        # Check if whitespace should be removed
+        whitespace_error_message = ''.join([
+            'whitespace must be a non-negative integer '
+            'with the minimal number of continous whitespace '
+            'to remove'
+        ])
+        if whitespace is not None:
+            # Make sure it's an integer
+            try:
+                whitespace = int(whitespace)
+            except TypeError:
+                raise error(whitespace_error_message)
+            # Make sure it's a non-negative integer
+            if whitespace < 0:
+                raise error(whitespace_error_message)
+            # Now remove whitespace
+            if whitespace == 0:
+                # Removing all whitespace from each value
+                for name in value_dict:
+                    value_dict[name] = re.sub('\s',  '', value_dict[name]) 
+            else:
+                # Removing only extra whitespace from each value
+                for name in value_dict:
+                    value_dict[name] = re.sub(
+                        '(\s)\s{%d,}' % (whitespace-1), '\\1', value_dict[name]
+                    ) 
+        # Finally, return values
+        return value_dict
+    except re.error, e:
+        # Something went wrong, we pass along the message from re module
+        raise error(e.message)
+    except AttributeError:
+        # No regexp match, just return an empty dictionary
+        return {}
